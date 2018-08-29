394266d2f47bdc12c0bca3ac5c51ef03db6e48c2
Nathan Tornquist <Tornquist@live.com>
2018-08-29 08:10:14 -0500

    Allow Categories to be saved and either created or updated

--- modules/Category.js
+++ modules/Category.js
@@ -5,8 +5,14 @@ let TimeError = require("./TimeError")
 module.exports = class Category {
 
   get name() { return this.props.name }
+  set name(newName) {
+    this._modifiedProps.push("name")
+    this.props.name = newName
+  }
 
   constructor(data = {})  {
+    this._modifiedProps = []
+
     this.id = data.id
     this.props = {
       parent_id: data.parent_id,
@@ -14,10 +20,46 @@ module.exports = class Category {
     }
   }
 
+  save() {
+    let neverSaved = this.id == null
+    let updatedFields = this._modifiedProps.length > 0
+
+    if (!(neverSaved || updatedFields)) { return this }
+
+    let db = require('../lib/db')()
+
+    let data = {}
+    if (neverSaved) {
+      data = {
+        name: this.name,
+        parent_id: this.props.parent_id
+      }
+    } else {
+      this._modifiedProps.forEach(prop => {
+        data[prop] = this.props[prop]
+      })
+    }
+
+    let action = neverSaved ?
+      db.insert(data).into('category') :
+      db('category').update(data).where('id', this.id)
+
+    return action.then(results => {
+      if (neverSaved) {
+        this.id = results[0]
+      } else {
+        this._modifiedProps = []
+      }
+
+      return this
+    })
+  }
+
   static async fetch(id) {
     let objectData = {}
     try {
       let data = await require('../lib/db')().select(
+        'id', // To avoid adding to data later
         'parent_id',
         'name'
       ).from('category')
@@ -28,7 +70,7 @@ module.exports = class Category {
         return Promise.reject(TimeError.Data.NOT_FOUND)
       }
 
-      let objectData = data[0]
+      objectData = data[0]
     } catch (error) {
       return Promise.reject(TimeError.Data.BAD_CONNECTION)
     }
--- test/test-category.js
+++ test/test-category.js
@@ -7,12 +7,32 @@ const config = require('./setup/config')
 const Time = require(process.env.PWD)(config)
 
 describe('Category Module', () => {
+  let newID;
+  let startingName = "Fun Stuff"
+  let endingName = "More Fun Stuff"
+
   describe('Creating a new category', () => {
     let category = new Time.Category()
 
     it('will start with no values', done => {
       should.equal(category.id, undefined);
       should.equal(category.name, undefined);
+      done()
+    })
+
+    it('allows name to be set', done => {
+      category.name = startingName
+      done()
+    })
+
+    it('can be saved', done => {
+      category.save()
+      .then(() => done()).catch(done)
+    })
+
+    it('will have a unique id', done => {
+      category.id.should.be.a('number')
+      newID = category.id
       done()
     })
   })
@@ -22,6 +42,38 @@ describe('Category Module', () => {
       it('will throw', () => {
         return Time.Category.fetch(1000)
         .should.be.rejectedWith(Time.Error.Data.NOT_FOUND)
+      })
+    })
+
+    describe('That does exist', () => {
+      let category;
+
+      before(async () => {
+        category = await Time.Category.fetch(newID)
+      })
+
+      it('will load with the expected values', done => {
+        category.name.should.eq(startingName)
+        done()
+      })
+
+      it('will allow name to be updated', done => {
+        category.name = endingName
+        category.name.should.eq(endingName)
+        category._modifiedProps.length.should.eq(1)
+        done()
+      })
+
+      it('will store the name with save', done => {
+        category.save()
+        .then(saved => Time.Category.fetch(category.id))
+        .then(freshObj => {
+          category.name.should.eq(endingName)
+          category._modifiedProps.length.should.eq(0)
+          freshObj.name.should.eq(endingName)
+          done()
+        })
+        .catch(done)
       })
     })
   })
