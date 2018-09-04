20f149dbd35311cff154d5712ce8dd3fe38633a2
Nathan Tornquist <Tornquist@live.com>
2018-09-04 08:20:50 -0500

    Migration for user table -- not yet connected to other tables

---
+++ migrations/sqls/20180904123628-user-up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user (
+  id serial primary key,
+  created_at timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
+
+  email varchar(255) NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  password_salt varchar(32) NOT NULL,
+
+  UNIQUE (email)
+);
+
+CREATE TRIGGER user_updated_at BEFORE UPDATE ON user FOR EACH ROW
+BEGIN
+  SET NEW.updated_at = NOW();
+END;
