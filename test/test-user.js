187025fc79af8b5c0b683615be3a753f5d4a2113
Nathan Tornquist <Tornquist@live.com>
2018-09-05 07:46:29 -0500

    Test user updates

--- test/test-user.js
+++ test/test-user.js
@@ -7,42 +7,56 @@ const config = require('./setup/config')
 const Time = require(process.env.PWD)(config)
 
 describe('User Module', () => {
-  describe('Creating a user', () => {
+  describe('Profile Operations', () => {
     let newUser;
 
     before(() => {
       newUser = new Time.User()
     })
 
-    it('rejects invalid email addresses', () => {
-      try {
-        newUser.email = "this_is_not_an_email"
-      } catch(e) {
-        e.should.eq(Time.Error.Data.INCORRECT_FORMAT)
-        return
-      }
-      throw new Error("Expected failure")
+    describe('Creating a user', () => {
+      it('rejects invalid email addresses', () => {
+        try {
+          newUser.email = "this_is_not_an_email"
+        } catch(e) {
+          e.should.eq(Time.Error.Data.INCORRECT_FORMAT)
+          return
+        }
+        throw new Error("Expected failure")
+      })
+
+      it('accepts valid email addresses', done => {
+        newUser.email = "email@webmail.com"
+        done()
+      })
+
+      it('rejects invalid passwords', () => {
+        return newUser.setPassword("short")
+        .should.be.rejectedWith(Time.Error.Data.INCORRECT_FORMAT)
+      })
+
+      it('accepts valid passwords', async () => {
+        let testPass = "myValidPassword"
+        return await newUser.setPassword(testPass)
+      })
+
+      it('can be created successfully', async () => {
+        await newUser.save()
+
+        newUser.id.should.be.a('number')
+      })
     })
 
-    it('accepts valid email addresses', done => {
-      newUser.email = "email@webmail.com"
-      done()
-    })
+    describe("Updating a user", () => {
+      it('allows email to be changed', async () => {
+        newUser.email = "email_2@webmail.com"
+        await newUser.save()
+      })
 
-    it('rejects invalid passwords', () => {
-      return newUser.setPassword("short")
-      .should.be.rejectedWith(Time.Error.Data.INCORRECT_FORMAT)
-    })
-
-    it('accepts valid passwords', async () => {
-      let testPass = "myValidPassword"
-      return await newUser.setPassword(testPass)
-    })
-
-    it('can be created successfully', async () => {
-      await newUser.save()
-
-      newUser.id.should.be.a('number')
+      it('allows password to be changed', async () => {
+        await newUser.setPassword("myValidPasswordNumberTwo")
+        await newUser.save()
+      })
     })
 
     after(async () => {
