43d9a264e10756235a22b926c5448ad541e9bf0a
Nathan Tornquist <Tornquist@live.com>
2018-10-13 10:11:21 -0500

    Add category_add procedure

---
+++ migrations/sqls/20181013143347-category-operations-down.sql
@@ -0,0 +1,1 @@
+DROP PROCEDURE IF EXISTS category_add;
---
+++ migrations/sqls/20181013143347-category-operations-up.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE category_add (
+  IN in_account_id BIGINT,
+  IN in_parent_id BIGINT,
+  IN in_name VARCHAR(30)
+)
+BEGIN
+  DECLARE new_lft INT;
+  SELECT rgt INTO new_lft FROM category WHERE id = in_parent_id AND account_id = in_account_id;
+  IF new_lft IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Category with requested parent_id and account_id not found';
+  END IF;
+  UPDATE category SET rgt = rgt + 2 WHERE rgt >= new_lft AND account_id = in_account_id;
+  UPDATE category SET lft = lft + 2 WHERE lft > new_lft AND account_id = in_account_id;
+  INSERT INTO category(name, lft, rgt, account_id) VALUES(in_name, new_lft, new_lft + 1, in_account_id);
+  SELECT LAST_INSERT_ID() as id;
+END;
