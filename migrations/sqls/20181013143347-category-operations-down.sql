b2290758462f7f4272b758955f374a73a2c7c7f1
Nathan Tornquist <Tornquist@live.com>
2018-10-14 20:48:37 -0500

    Add category_delete

--- migrations/sqls/20181013143347-category-operations-down.sql
+++ migrations/sqls/20181013143347-category-operations-down.sql
@@ -1,2 +1,3 @@
+DROP PROCEDURE IF EXISTS category_delete;
 DROP PROCEDURE IF EXISTS category_add;
 DROP PROCEDURE IF EXISTS category_visualize;
--- migrations/sqls/20181013143347-category-operations-up.sql
+++ migrations/sqls/20181013143347-category-operations-up.sql
@@ -4,7 +4,9 @@ CREATE PROCEDURE category_visualize (
 BEGIN
   SELECT
     node.id,
-    CONCAT( REPEAT('    ', COUNT(parent.name) - 1), node.name) AS name
+    node.lft,
+    node.rgt,
+    CONCAT( REPEAT('|    ', COUNT(parent.name) - 1), node.name) AS name
   FROM
     category AS node,
     category AS parent
@@ -32,4 +34,39 @@ BEGIN
   UPDATE category SET lft = lft + 2 WHERE lft > new_lft AND account_id = in_account_id;
   INSERT INTO category(name, lft, rgt, account_id) VALUES(in_name, new_lft, new_lft + 1, in_account_id);
   SELECT LAST_INSERT_ID() as id;
+END;
+
+CREATE PROCEDURE category_delete (
+  IN in_parent_id BIGINT,
+  IN in_delete_children BOOLEAN
+)
+BEGIN
+  DECLARE n_lft INT;
+  DECLARE n_rgt INT;
+  DECLARE n_width BOOLEAN;
+  DECLARE n_account_id BIGINT;
+
+  SELECT
+    rgt - lft, lft, rgt, account_id
+  INTO
+    n_width, n_lft, n_rgt, n_account_id
+  FROM category WHERE id = in_parent_id;
+
+  IF n_lft IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Category with requested parent_id not found';
+  END IF;
+
+  IF n_width = 1 OR in_delete_children THEN
+    DELETE FROM category WHERE lft BETWEEN n_lft AND n_rgt;
+
+    UPDATE category SET rgt = rgt - (n_width + 1) WHERE rgt > n_rgt AND account_id = n_account_id;
+    UPDATE category SET lft = lft - (n_width + 1) WHERE lft > n_rgt AND account_id = n_account_id;
+  ELSE
+    DELETE FROM category WHERE id = in_parent_id;
+
+    UPDATE category SET rgt = rgt - 1, lft = lft - 1 WHERE lft BETWEEN n_lft AND n_rgt AND account_id = n_account_id;
+    UPDATE category SET rgt = rgt - 2 WHERE rgt > n_rgt AND account_id = n_account_id;
+    UPDATE category SET lft = lft - 2 WHERE lft > n_rgt AND account_id = n_account_id;
+  END IF;
 END;
