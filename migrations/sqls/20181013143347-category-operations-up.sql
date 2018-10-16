04eac84b7bb6887f55a0a40bbac4f4b8ad12a664
Nathan Tornquist <Tornquist@live.com>
2018-10-15 21:26:38 -0500

    Make naming of variables and inputs consistent

--- migrations/sqls/20181013143347-category-operations-up.sql
+++ migrations/sqls/20181013143347-category-operations-up.sql
@@ -24,46 +24,47 @@ CREATE PROCEDURE category_add (
   IN in_name VARCHAR(30)
 )
 BEGIN
-  DECLARE new_lft INT;
-  SELECT rgt INTO new_lft FROM category WHERE id = in_parent_id AND account_id = in_account_id;
-  IF new_lft IS NULL THEN
+  DECLARE n_new_lft INT;
+  DECLARE n_new_rgt INT;
+  SELECT rgt, rgt + 1 INTO n_new_lft, n_new_rgt FROM category WHERE id = in_parent_id AND account_id = in_account_id;
+  IF n_new_lft IS NULL THEN
     SIGNAL SQLSTATE '45000'
     SET MESSAGE_TEXT = 'Category with requested parent_id and account_id not found';
   END IF;
-  UPDATE category SET rgt = rgt + 2 WHERE rgt >= new_lft AND account_id = in_account_id;
-  UPDATE category SET lft = lft + 2 WHERE lft > new_lft AND account_id = in_account_id;
-  INSERT INTO category(name, lft, rgt, account_id) VALUES(in_name, new_lft, new_lft + 1, in_account_id);
+  UPDATE category SET rgt = rgt + 2 WHERE rgt >= n_new_lft AND account_id = in_account_id;
+  UPDATE category SET lft = lft + 2 WHERE lft > n_new_lft AND account_id = in_account_id;
+  INSERT INTO category(name, lft, rgt, account_id) VALUES(in_name, n_new_lft, n_new_rgt, in_account_id);
   SELECT LAST_INSERT_ID() as id;
 END;
 
 CREATE PROCEDURE category_delete (
-  IN in_parent_id BIGINT,
+  IN in_node_id BIGINT,
   IN in_delete_children BOOLEAN
 )
 BEGIN
   DECLARE n_lft INT;
   DECLARE n_rgt INT;
-  DECLARE n_width BOOLEAN;
+  DECLARE n_width INT;
   DECLARE n_account_id BIGINT;
 
   SELECT
-    rgt - lft, lft, rgt, account_id
+    rgt - lft + 1, lft, rgt, account_id
   INTO
     n_width, n_lft, n_rgt, n_account_id
-  FROM category WHERE id = in_parent_id;
+  FROM category WHERE id = in_node_id;
 
   IF n_lft IS NULL THEN
     SIGNAL SQLSTATE '45000'
     SET MESSAGE_TEXT = 'Category with requested parent_id not found';
   END IF;
 
-  IF n_width = 1 OR in_delete_children THEN
+  IF n_width = 2 OR in_delete_children THEN
     DELETE FROM category WHERE lft BETWEEN n_lft AND n_rgt;
 
-    UPDATE category SET rgt = rgt - (n_width + 1) WHERE rgt > n_rgt AND account_id = n_account_id;
-    UPDATE category SET lft = lft - (n_width + 1) WHERE lft > n_rgt AND account_id = n_account_id;
+    UPDATE category SET rgt = rgt - n_width WHERE rgt > n_rgt AND account_id = n_account_id;
+    UPDATE category SET lft = lft - n_width WHERE lft > n_rgt AND account_id = n_account_id;
   ELSE
-    DELETE FROM category WHERE id = in_parent_id;
+    DELETE FROM category WHERE id = in_node_id;
 
     UPDATE category SET rgt = rgt - 1, lft = lft - 1 WHERE lft BETWEEN n_lft AND n_rgt AND account_id = n_account_id;
     UPDATE category SET rgt = rgt - 2 WHERE rgt > n_rgt AND account_id = n_account_id;
