7e65fc532956dd67d7278444dcc18995d1774a9b
Nathan Tornquist <Tornquist@live.com>
2019-12-09 07:27:08 -0600

    Expand tests and validate imported status

--- test/test-import.js
+++ test/test-import.js
@@ -13,12 +13,17 @@ const testData = require('./data/import')
 const sleep = (ms) => (new Promise(resolve => setTimeout(resolve, ms)))
 
 describe('Import Module', () => {
-  let user = null;
+  let user1 = null;
+  let user2 = null;
+  let largeImporterID = null;
 
   before(async () => {
     UserHelper.link(Time)
-    user = await UserHelper.create()
-    user = user.user
+    let user1Data = await UserHelper.create()
+    user1 = user1Data.user
+
+    let user2Data = await UserHelper.create()
+    user2 = user2Data.user
   })
 
   describe('Request formatting', () => {
@@ -93,7 +98,7 @@ describe('Import Module', () => {
 
   describe('Loading Data', () => {
     it('rejects invalid formatting', () => {
-      return Time.Import.loadInto(user, {
+      return Time.Import.loadInto(user1, {
         name: 'Missing keys'
       })
       .should.be.rejectedWith(Time.Error.Data.INCORRECT_FORMAT)
@@ -101,7 +106,7 @@ describe('Import Module', () => {
 
     it('validates names: only top level can be empty', async () => {
       // Accept empty top
-      await Time.Import.loadInto(user, {
+      await Time.Import.loadInto(user1, {
         name: '',
         events: [],
         ranges: [],
@@ -109,7 +114,7 @@ describe('Import Module', () => {
       }).should.be.fulfilled
 
       // Reject empty child
-      await Time.Import.loadInto(user, {
+      await Time.Import.loadInto(user1, {
         name: '',
         events: [],
         ranges: [],
@@ -122,7 +127,7 @@ describe('Import Module', () => {
       }).should.be.rejectedWith(Time.Error.Data.INCORRECT_FORMAT)
 
       // Accept set child
-      await Time.Import.loadInto(user, {
+      await Time.Import.loadInto(user1, {
         name: '',
         events: [],
         ranges: [],
@@ -135,7 +140,7 @@ describe('Import Module', () => {
       }).should.be.fulfilled
 
       // Accept set root and child
-      await Time.Import.loadInto(user, {
+      await Time.Import.loadInto(user1, {
         name: 'Parent name',
         events: [],
         ranges: [],
@@ -148,7 +153,7 @@ describe('Import Module', () => {
       }).should.be.fulfilled
 
       // Accept empty root and set children
-      await Time.Import.loadInto(user, {
+      await Time.Import.loadInto(user1, {
         name: '',
         events: [],
         ranges: [],
@@ -174,7 +179,7 @@ describe('Import Module', () => {
       }).should.be.fulfilled
 
       // Reject empty inner child
-      await Time.Import.loadInto(user, {
+      await Time.Import.loadInto(user1, {
         name: '',
         events: [],
         ranges: [],
@@ -193,7 +198,7 @@ describe('Import Module', () => {
     })
 
     it('returns a import object', async () => {
-      let importReference = await Time.Import.loadInto(user, {
+      let importReference = await Time.Import.loadInto(user1, {
         name: '',
         events: [],
         ranges: [],
@@ -214,7 +219,7 @@ describe('Import Module', () => {
       })
 
       importReference.constructor.name.should.eq('Import')
-      importReference.userID.should.eq(user.id)
+      importReference.userID.should.eq(user1.id)
       importReference.createdAt.should.be.a('date')
       importReference.updatedAt.should.be.a('date')
       importReference.expectedCategories.should.eq(1)
@@ -224,76 +229,194 @@ describe('Import Module', () => {
       importReference.complete.should.eq(false)
       importReference.success.should.eq(false)
     })
+  })
 
-    describe('Loading large files', () => {
-      let importer;
+  describe('Loading large files', () => {
+    let importer;
 
-      it('Allows an import to be initiated', async () => {
-        importer = await Time.Import.loadInto(user, testData)
+    it('allows an import to be initiated', async () => {
+      importer = await Time.Import.loadInto(user2, testData)
+      largeImporterID = importer.id
 
-        importer.constructor.name.should.eq('Import')
-        importer.userID.should.eq(user.id)
-        importer.createdAt.should.be.a('date')
-        importer.updatedAt.should.be.a('date')
-        importer.expectedCategories.should.eq(9)
-        importer.importedCategories.should.eq(0)
-        importer.expectedEntries.should.eq(138)
-        importer.importedEntries.should.eq(0)
-        importer.complete.should.eq(false)
-        importer.success.should.eq(false)
-      })
+      importer.constructor.name.should.eq('Import')
+      importer.userID.should.eq(user2.id)
+      importer.createdAt.should.be.a('date')
+      importer.updatedAt.should.be.a('date')
+      importer.expectedCategories.should.eq(9)
+      importer.importedCategories.should.eq(0)
+      importer.expectedEntries.should.eq(138)
+      importer.importedEntries.should.eq(0)
+      importer.complete.should.eq(false)
+      importer.success.should.eq(false)
+    })
 
-      it('Updates progress as it executes',  async () => {
-        let lastImportedEntries = importer.importedEntries
-        let lastImportedCategories = importer.importedCategories
+    it('updates progress as it executes',  async () => {
+      let lastImportedEntries = importer.importedEntries
+      let lastImportedCategories = importer.importedCategories
 
-        let waitForCategoryComplete = true
-        while (waitForCategoryComplete) {
-          await sleep(10)
-          if (importer.importedCategories === importer.expectedCategories) {
-            waitForCategoryComplete = false
-          }
+      let waitForCategoryComplete = true
+      while (waitForCategoryComplete) {
+        await sleep(10)
+        if (importer.importedCategories === importer.expectedCategories) {
+          waitForCategoryComplete = false
         }
+      }
 
-        for (let i = 0; i < 3; i++) {
-          await sleep(25)
+      for (let i = 0; i < 3; i++) {
+        await sleep(30)
 
-          importer.importedEntries.should.be.greaterThan(lastImportedEntries)
-          importer.importedCategories.should.be.greaterThan(lastImportedCategories)
-        }
+        importer.importedEntries.should.be.greaterThan(lastImportedEntries)
+        importer.importedCategories.should.be.greaterThan(lastImportedCategories)
+      }
+    })
+
+    it('marks complete when finished', async () => {
+      while (importer.complete !== true) {
+        await sleep(10)
+      }
+
+      importer.constructor.name.should.eq('Import')
+      importer.userID.should.eq(user2.id)
+      importer.createdAt.should.be.a('date')
+      importer.updatedAt.should.be.a('date')
+      importer.expectedCategories.should.eq(9)
+      importer.importedCategories.should.eq(9)
+      importer.expectedEntries.should.eq(138)
+      importer.importedEntries.should.eq(138)
+      importer.complete.should.eq(true)
+      importer.success.should.eq(true)
+    })
+
+    it('imports the expected data', async () => {
+      let accounts = await Time.Account.findForUser(user2)
+      accounts.length.should.eq(1)
+      let account = accounts[0]
+
+      let categories = await Time.Category.findForAccount(account)
+      categories.length.should.eq(9 + 1) // Imported + Root
+      
+      let root = categories.find(c => c.name === 'root')
+      let children = await root.getChildren()
+      let childrenNames = Array.from(new Set(children.map(c => c.name)))
+      let expectedNames = Array.from(new Set(['Life', 'Work', 'Side Projects']))
+      childrenNames.length.should.eq(expectedNames.length)
+      for (let name of childrenNames) {
+        expectedNames.should.include(name)
+      }
+
+      let entries = await Time.Entry.findFor({ accounts: [account] })
+      entries.length.should.eq(138)
+    })
+  })
+
+  describe('Fetching import records', () => {
+    it('can be loaded directly', async () => {
+      let fetchedImporter = await Time.Import.fetch(largeImporterID)
+
+      fetchedImporter.constructor.name.should.eq('Import')
+      fetchedImporter.userID.should.eq(user2.id)
+      fetchedImporter.createdAt.should.be.a('date')
+      fetchedImporter.updatedAt.should.be.a('date')
+      fetchedImporter.expectedCategories.should.eq(9)
+      fetchedImporter.importedCategories.should.eq(9)
+      fetchedImporter.expectedEntries.should.eq(138)
+      fetchedImporter.importedEntries.should.eq(138)
+      fetchedImporter.complete.should.eq(true)
+      fetchedImporter.success.should.eq(true)
+    })
+
+    it('only allows save to be called once', async () => {
+      let fetchedImporter = await Time.Import.fetch(largeImporterID)
+      try {
+        await fetchedImporter.save()
+      } catch (err) {
+        err.should.eq(Time.Error.Request.INVALID_ACTION)
+        return
+      }
+      throw new Error('Should have thrown Time.Error.Request.INVALID_ACTION')
+    })
+
+    it('throws when loading invalid IDs', () => {
+      return Time.Import.fetch(10000)
+        .should.be.rejectedWith(Time.Error.Data.NOT_FOUND)
+    })
+  })
+
+  describe('Importing data with set and null roots', () => {
+    it('attaches a set root node directly to the account root', async () => {
+      let user = await UserHelper.create()
+      user = user.user
+
+      await Time.Import.loadInto(user, {
+        name: 'Set Root',
+        events: [],
+        ranges: [],
+        children: [{
+          name: 'First Child',
+          events: [],
+          ranges: [],
+          children: []
+        }]
       })
 
-      it('Marks complete when finished', async () => {
-        while (importer.complete !== true) {
-          await sleep(10)
-        }
+      await sleep(100)
 
-        importer.constructor.name.should.eq('Import')
-        importer.userID.should.eq(user.id)
-        importer.createdAt.should.be.a('date')
-        importer.updatedAt.should.be.a('date')
-        importer.expectedCategories.should.eq(9)
-        importer.importedCategories.should.eq(9)
-        importer.expectedEntries.should.eq(138)
-        importer.importedEntries.should.eq(138)
-        importer.complete.should.eq(true)
-        importer.success.should.eq(true)
+      let accounts = await Time.Account.findForUser(user)
+      accounts.length.should.eq(1)
+      let account = accounts[0]
+
+      let categories = await Time.Category.findForAccount(account)
+      categories.length.should.eq(2 + 1) // Imported + Root
+      let root = categories.find(c => c.name === 'root')
+      let children = await root.getChildren()
+      let childrenNames = children.map(c => c.name)
+
+      childrenNames.length.should.eq(1)
+      childrenNames[0].should.eq('Set Root')
+    })
+
+    it('attaches the children of the null root directly to the account root', async () => {
+      let user = await UserHelper.create()
+      user = user.user
+
+      await Time.Import.loadInto(user, {
+        name: '', // Null Root
+        events: [],
+        ranges: [],
+        children: [{
+          name: 'First Child',
+          events: [],
+          ranges: [],
+          children: []
+        }, {
+          name: 'Second Child',
+          events: [],
+          ranges: [],
+          children: []
+        }, {
+          name: 'Third Child',
+          events: [],
+          ranges: [],
+          children: []
+        }]
       })
 
-      it('Can be fetched and validated', async () => {
-        let fetchedImporter = await Time.Import.fetch(importer.id)
+      await sleep(100)
 
-        fetchedImporter.constructor.name.should.eq('Import')
-        fetchedImporter.userID.should.eq(user.id)
-        fetchedImporter.createdAt.should.be.a('date')
-        fetchedImporter.updatedAt.should.be.a('date')
-        fetchedImporter.expectedCategories.should.eq(9)
-        fetchedImporter.importedCategories.should.eq(9)
-        fetchedImporter.expectedEntries.should.eq(138)
-        fetchedImporter.importedEntries.should.eq(138)
-        fetchedImporter.complete.should.eq(true)
-        fetchedImporter.success.should.eq(true)
-      })
+      let accounts = await Time.Account.findForUser(user)
+      accounts.length.should.eq(1)
+      let account = accounts[0]
+
+      let categories = await Time.Category.findForAccount(account)
+      categories.length.should.eq(3 + 1) // Imported + Root
+      let root = categories.find(c => c.name === 'root')
+      let children = await root.getChildren()
+      let childrenNames = children.map(c => c.name)
+
+      childrenNames.length.should.eq(3)
+      childrenNames.should.include('First Child')
+      childrenNames.should.include('Second Child')
+      childrenNames.should.include('Third Child')
     })
   })
 })
